cc58b9d623ccff882c46d42dbb0e760ccf3801a4
JasonDroste-thoracic <jdroste@thoracic.org>
2024-02-18 19:51:37 -0600

    Modified Meta table

--- database/migrations/2024_02_17_231046_create_metatypes_table.php
+++ database/migrations/2024_02_17_231046_create_metatypes_table.php
@@ -13,8 +13,6 @@ return new class extends Migration
     {
         Schema::create('metatypes', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
-            $table->string('name');
             $table->string('meta_type');
             $table->integer('BP');
             $table->integer('BOD');
