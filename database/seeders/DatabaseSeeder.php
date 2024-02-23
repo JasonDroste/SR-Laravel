98137f97b47189d0130babc8ffa45ddfc0874f7d
JasonDroste-thoracic <jdroste@thoracic.org>
2024-02-22 18:53:14 -0600

    Corrected ID issue

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\MetaTypes;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,7 +13,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+       // \App\Models\User::factory(2)->create();
+
+
+       // MetaTypes::create( ['meta_type' => 'Human','BP' => 0, 'BOD' => 1, 'AGI' => 1, 'REA' => 1, 'STR' => 1, 'CHA' => 1, 'INTL' => 1, 'LOG' => 1, 'WIL' => 1, 'INI' => 2, 'meta_ability' => '+1 edge']);
+
+        //MetaTypes::create(['meta_type' => 'Ork','BP' => -20, 'BOD' => 4, 'AGI' => 1, 'REA' => 1, 'STR' => 3, 'CHA' => 1, 'INTL' => 1, 'LOG' => 1, 'WIL' => 1, 'INI' => 2, 'meta_ability' => 'Low-light Vision']);
+
+        //MetaTypes::create(['meta_type' => 'Dwarf','BP' => -25, 'BOD' => 2, 'AGI' => 1, 'REA' => 1, 'STR' => 3, 'CHA' => 1, 'INTL' => 1, 'LOG' => 1, 'WIL' => 2, 'INI' => 2, 'meta_ability' => 'Thermographic  Vision, +2 dice for body tests to resist pathogens and toxins']);
+
+        //MetaTypes::create(['meta_type' => 'Elf','BP' => -30, 'BOD' => 1, 'AGI' => 2, 'REA' => 1, 'STR' => 1, 'CHA' => 3, 'INTL' => 1, 'LOG' => 1, 'WIL' => 2, 'INI' => 2, 'meta_ability' => 'Low-Light Vision']);
+
+        //MetaTypes::create(['meta_type' => 'Troll','BP' => -40, 'BOD' => 5, 'AGI' => 1, 'REA' => 1, 'STR' => 5, 'CHA' => 1, 'INTL' => 1, 'LOG' => 1, 'WIL' => 1, 'INI' => 2, 'meta_ability' => 'Thermographic vision +1 reach, +1 natural armor (cumulative with worn armor)']);
+
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
